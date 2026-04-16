5f0e4316c29971fd85817b8943e545868b028300
Leander Funken <leander.funken@gmail.com>
2026-04-16 09:17:42 +0200

    chore: format some code

--- Src/Libs/Platform/BHAssert.h
+++ Src/Libs/Platform/BHAssert.h
@@ -12,52 +12,52 @@
 #undef VERIFY
 
 #include <string>
-#include <sstream>
 
 /**
  * Some tools for low level debugging
  */
-class Assert
-{
-public:
-  /**
-   * Prints a formatted message to stdout
-   * @param file The name of the current file (__FILE__)
-   * @param line The current file line (__LINE__)
-   * @param format The format of the message to be printed
-   */
-  static void print(const char* file, int line, const char* format, ...);
-  static void print(const char* file, int line, const std::string& message);
+class Assert {
+   public:
+    /**
+     * Prints a formatted message to stdout
+     * @param file The name of the current file (__FILE__)
+     * @param line The current file line (__LINE__)
+     * @param format The format of the message to be printed
+     */
+    static void print(const char* file, int line, const char* format, ...);
+    static void print(const char* file, int line, const std::string& message);
 
-  /**
-   * Aborts the execution of the program
-   */
-  static void abort();
+    /**
+     * Aborts the execution of the program
+     */
+    static void abort();
 
 #ifdef TARGET_ROBOT
-  /**
-   * Initializes some log message ring buffers associated to the current thread
-   * @param name The name of the current thread
-   * @return Whether the initialization was successful
-   */
-  static bool logInit(const char* name);
+    /**
+     * Initializes some log message ring buffers associated to the current thread
+     * @param name The name of the current thread
+     * @return Whether the initialization was successful
+     */
+    static bool logInit(const char* name);
 
-  /**
-   * Adds a message to the log message ring buffer
-   * @param track The id of the used ring buffer
-   * @param file The name of the current file (__FILE__)
-   * @param line The current file line (__LINE__)
-   * @param message The message to be added
-   */
-  static void logAdd(int track, const char* file, int line, const std::string& message);
+    /**
+     * Adds a message to the log message ring buffer
+     * @param track The id of the used ring buffer
+     * @param file The name of the current file (__FILE__)
+     * @param line The current file line (__LINE__)
+     * @param message The message to be added
+     */
+    static void logAdd(int track, const char* file, int line, const std::string& message);
 
-  /**
-   * Dumps the content of the log message ring buffers to stderr or /home/nao/bhdump.log
-   * @param toStderr Whether to dump the content to stderr or /home/nao/bhdump.log
-   * @param termSignal A signal that was emitted to terminate the bhuman process
-   */
-  static void logDump(bool toStderr, int termSignal);
-#endif // TARGET_ROBOT
+    /**
+     * Dumps the content of the log message ring buffers to stderr or
+     * /home/nao/bhdump.log
+     * @param toStderr Whether to dump the content to stderr or
+     * /home/nao/bhdump.log
+     * @param termSignal A signal that was emitted to terminate the bhuman process
+     */
+    static void logDump(bool toStderr, int termSignal);
+#endif  // TARGET_ROBOT
 };
 
 /**
@@ -68,25 +68,34 @@ public:
 #ifdef NDEBUG
 #define ASSERT(cond) static_cast<void>(0)
 #elif defined TARGET_ROBOT
-#define ASSERT(cond) static_cast<void>((cond) ? 0 : (Assert::logAdd(0, __FILE__, __LINE__, "ASSERT(" #cond ") failed"), Assert::abort(), 0))
+#define ASSERT(cond)                                                                               \
+    static_cast<void>((cond) ? 0                                                                   \
+                             : (Assert::logAdd(0, __FILE__, __LINE__, "ASSERT(" #cond ") failed"), \
+                                Assert::abort(), 0))
 #else
-#define ASSERT(cond) static_cast<void>((cond) ? 0 : (Assert::print(__FILE__, __LINE__, "ASSERT(%s) failed", #cond), Assert::abort(), 0))
+#define ASSERT(cond)                                                                           \
+    static_cast<void>((cond) ? 0                                                               \
+                             : (Assert::print(__FILE__, __LINE__, "ASSERT(%s) failed", #cond), \
+                                Assert::abort(), 0))
 #endif
 
 /**
  * ASSERTs cond
- * if NDEBUG is defined the safe value will be returned in the case of a failing conditional check
- * it is recommended to use SAFE_ASSERT just for cond that are fast evaluated
+ * if NDEBUG is defined the safe value will be returned in the case of a failing
+ * conditional check it is recommended to use SAFE_ASSERT just for cond that are
+ * fast evaluated
  */
 #ifdef NDEBUG
-#define SAFE_ASSERT(cond, safe) while(!(cond)) return (safe)
+#define SAFE_ASSERT(cond, safe) \
+    while (!(cond)) return (safe)
 #else
 #define SAFE_ASSERT(cond, safe) ASSERT(cond)
 #endif
 
 /**
  * FAIL is equivalent to ASSERT(false) and additionally prints a text.
- * This text is passed into a std::stringstream, thus FAIL("error " << 1) is a valid expression.
+ * This text is passed into a std::stringstream, thus FAIL("error " << 1) is a
+ * valid expression.
  */
 #ifdef NDEBUG
 #define FAIL(text) static_cast<void>(0)
@@ -96,15 +105,13 @@ public:
 #else
 #define _FAIL_OUTPUT Assert::print(__FILE__, __LINE__, sstream.str());
 #endif
-#define FAIL(text) \
-  do \
-  { \
-    std::stringstream sstream; \
-    sstream << "FAIL: " << text; \
-    _FAIL_OUTPUT \
-    Assert::abort(); \
-  } \
-  while(false)
+#define FAIL(text)                   \
+    do {                             \
+        std::stringstream sstream;   \
+        sstream << "FAIL: " << text; \
+        _FAIL_OUTPUT                 \
+        Assert::abort();             \
+    } while (false)
 #endif
 
 /**
@@ -115,9 +122,15 @@ public:
 #ifdef NDEBUG
 #define VERIFY(cond) static_cast<void>(cond)
 #elif defined TARGET_ROBOT
-#define VERIFY(cond) static_cast<void>((cond) ? 0 : (Assert::logAdd(0, __FILE__, __LINE__, "VERIFY(" #cond ") failed"), Assert::abort(), 0))
+#define VERIFY(cond)                                                                               \
+    static_cast<void>((cond) ? 0                                                                   \
+                             : (Assert::logAdd(0, __FILE__, __LINE__, "VERIFY(" #cond ") failed"), \
+                                Assert::abort(), 0))
 #else
-#define VERIFY(cond) static_cast<void>((cond) ? 0 : (Assert::print(__FILE__, __LINE__, "VERIFY(%s) failed", #cond), Assert::abort(), 0))
+#define VERIFY(cond)                                                                           \
+    static_cast<void>((cond) ? 0                                                               \
+                             : (Assert::print(__FILE__, __LINE__, "VERIFY(%s) failed", #cond), \
+                                Assert::abort(), 0))
 #endif
 
 /**
@@ -157,7 +170,8 @@ public:
 #define _WARNING(text) #text
 #ifdef WINDOWS
 #define _WARNING2(text) _WARNING(text)
-#define WARNING(text) _Pragma(_WARNING(message (__FILE__ "(" _WARNING2(__LINE__) "): warning: " text)))
+#define WARNING(text) \
+    _Pragma(_WARNING(message(__FILE__ "(" _WARNING2(__LINE__) "): warning: " text)))
 #else
 #define WARNING(text) _Pragma(_WARNING(GCC warning text))
 #endif
